70fb31b23c14d72483933a0f77183fc99d94a64c
Nahla Nagib <95168510+nahnagib@users.noreply.github.com>
2023-09-07 22:47:43 +0200

    Add files via upload

---
+++ go_shopping.py
@@ -0,0 +1,32 @@
+#3 functions to print summation, avg , multiplication for 2 values
+def sum(a,r):
+    su=a+r
+    return su 
+def avg(a,r):
+    av=sum(a,r)/2
+    return av
+def mullt(a,r):
+    mu=a*r
+    return mu
+a=int(input("enter first number: "))
+b=int(input("enter second number:"))
+
+print(sum,avg,mullt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
